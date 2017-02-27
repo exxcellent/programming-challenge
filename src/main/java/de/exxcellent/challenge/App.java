2632ebb3c5c8128b0f4c3eb115af4763f4919190
Benjamin Schmid <benjamin.schmid@exxcellent.de>
2017-02-27 19:48:03 +0100

    feat(Nested Lists): provide basic nested list example

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,10 @@
 package de.exxcellent.challenge;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import static java.util.Arrays.asList;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -8,7 +13,16 @@ package de.exxcellent.challenge;
  */
 public final class App {
 
+    public static final Collection<Object> EXAMPLE_NESTED_LIST = Collections.unmodifiableList(
+            asList(1, asList(2, 3), asList(4, asList(5, 6)))
+    );
+
+
     public static void main(String... args) {
-        System.out.println("Hello world!");
+        List<Integer> result = asList(-1, -2); // yourFunction(EXAMPLE_NESTED_LIST)
+
+        System.out.print(EXAMPLE_NESTED_LIST);
+        System.out.print(" -> ");
+        System.out.println(result);
     }
 }
