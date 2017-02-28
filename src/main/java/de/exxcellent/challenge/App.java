4e12147d10de238766f9019fd4a1444271ad56a4
Benjamin Schmid <benjamin.schmid@exxcellent.de>
2017-02-28 19:42:02 +0100

    doc(README & App): add hints & notes on challenge

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -9,6 +9,13 @@ package de.exxcellent.challenge;
 public final class App {
 
     public static void main(String... args) {
-        System.out.println("Hello world!");
+
+        // Your preparation code …
+
+        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        String teamWithSmallesGoalSpread = "A good team"; // Your goal analysis function call …
+
+        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallesGoalSpread);
     }
 }
