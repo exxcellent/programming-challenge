454986d760e3ead44788a6483320139611c5fcd6
Benjamin Schmid <benjamin.schmid@exxcellent.de>
2018-11-22 15:55:47 +0100

    docs(package-info): introduce package docs to satisfy codestyle guidelines

---
+++ src/main/java/de/exxcellent/challenge/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The root package for the data challenge containing the entry point
+ * {@link de.exxcellent.challenge.App} and containing the resource files.
+ * Please create and document your own package structure appropriate to your design.
+ *
+ * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ */
+package de.exxcellent.challenge;
